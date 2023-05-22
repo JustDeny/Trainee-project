5320b95418235c435d7d9665bbb515f43ebf2ef8
JustDeny <denchik.eroshenko@gmail.com>
2023-05-22 19:37:58 +0300

    add zoom in/out, changing pages, opening file

--- PdfReader/PdfReaderWindow.cpp
+++ PdfReader/PdfReaderWindow.cpp
@@ -2,17 +2,18 @@
 #include <qmessagebox.h>
 #include <iostream>
 
+const qreal zoomMultiplier = qSqrt(2.0);
 
 PdfReaderWindow::PdfReaderWindow(QWidget* parent)
     : QMainWindow(parent), m_document(new QPdfDocument(this)),
     central_widget(new QWidget(this)),
-    splitter(new QSplitter(Qt::Horizontal,central_widget)),
+    splitter(new QSplitter(Qt::Horizontal, central_widget)),
     tool_bar(new QToolBar(this)),
     menu_bar(new QMenuBar(this)),
     m_zoomSelector(new ZoomSelector(this)),
     m_pageSelector(new QSpinBox(this)),
     tab_widget(new QTabWidget(this)),
-    pdf_view_widget(new QPdfView()), 
+    pdf_view_widget(new QPdfView()),
     h_layout(new QHBoxLayout(central_widget))
 {
     resize(init_width, init_height);
@@ -75,11 +76,13 @@ void PdfReaderWindow::setupUI()
     tool_bar->setMovable(false);
     QAction* open_tool = tool_bar->addAction("Open File");
     open_tool->setIcon(QIcon("images/document-open.svgz"));
+    tool_bar->addSeparator();
     QAction* zoom_out_tool = tool_bar->addAction("Zoom Out");
     zoom_out_tool->setIcon(QIcon("images/zoom-out.svgz"));
     tool_bar->addWidget(m_zoomSelector);
     QAction* zoom_in_tool = tool_bar->addAction("Zoom In");
     zoom_in_tool->setIcon(QIcon("images/zoom-in.svgz"));
+    tool_bar->addSeparator();
     QAction* prev_page_tool = tool_bar->addAction("Previous Page");
     prev_page_tool->setIcon(QIcon("images/go-previous-view.svgz"));
     tool_bar->addWidget(m_pageSelector);
@@ -87,6 +90,17 @@ void PdfReaderWindow::setupUI()
     next_page_tool->setIcon(QIcon("images/go-next-view.svgz"));
 
 
+    //connecting signals for tool bar
+    connect(open_tool, &QAction::triggered, this, &PdfReaderWindow::on_actionopen_triggered);
+    connect(zoom_in_tool, &QAction::triggered, this, &PdfReaderWindow::on_actionzoom_in_triggered);
+    connect(zoom_out_tool, &QAction::triggered, this, &PdfReaderWindow::on_actionzoom_out_triggered);
+    connect(prev_page_tool, &QAction::triggered, this, &PdfReaderWindow::on_actionprevious_page_triggered);
+    connect(next_page_tool, &QAction::triggered, this, &PdfReaderWindow::on_actionnext_page_triggered);
+
+    //connecting signals for zoom selector
+    connect(m_zoomSelector, &ZoomSelector::zoomModeChanged, pdf_view_widget, &QPdfView::setZoomMode);
+    connect(m_zoomSelector, &ZoomSelector::zoomFactorChanged, pdf_view_widget, &QPdfView::setZoomFactor);
+    m_zoomSelector->reset();
 }
 
 void PdfReaderWindow::open(const QUrl& docLocation)
@@ -98,7 +112,7 @@ void PdfReaderWindow::open(const QUrl& docLocation)
         pageSelected(0);
         m_pageSelector->setMaximum(m_document->pageCount() - 1);
     }
-    else{
+    else {
         const QString message = tr("%1 is not a valid local file").arg(docLocation.toString());
         QMessageBox::critical(this, tr("Failed to open"), message);
     }
@@ -136,4 +150,26 @@ void PdfReaderWindow::on_actionabout_triggered()
     AboutDialog dialog(this);
     dialog.exec();
 }
-    
+
+void PdfReaderWindow::on_actionzoom_in_triggered()
+{
+    pdf_view_widget->setZoomFactor(pdf_view_widget->zoomFactor() * zoomMultiplier);
+
+}
+
+void PdfReaderWindow::on_actionzoom_out_triggered()
+{
+    pdf_view_widget->setZoomFactor(pdf_view_widget->zoomFactor() / zoomMultiplier);
+}
+
+void PdfReaderWindow::on_actionprevious_page_triggered()
+{
+    auto nav = pdf_view_widget->pageNavigator();
+    nav->jump(nav->currentPage() - 1, {}, nav->currentZoom());
+}
+
+void PdfReaderWindow::on_actionnext_page_triggered()
+{
+    auto nav = pdf_view_widget->pageNavigator();
+    nav->jump(nav->currentPage() + 1, {}, nav->currentZoom());
+}
